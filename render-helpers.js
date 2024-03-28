d0208b4f10f6009560ab74849a4d6b148a90373f
isabellaucani <129698947+isabellaucani@users.noreply.github.com>
2024-03-28 13:11:51 -0400

    Update render-helpers.js

--- render-helpers.js
+++ render-helpers.js
@@ -77,7 +77,7 @@ export function renderWithProvider(component, store, pathname = '') {
 
 export function renderHookWithProvider(hook, state, pathname = '/') {
   const store = state ? configureStore(state) : undefined;
-  const { history, Wrapper } = createProviderWrapper(store, pathname);
+  const {Wrapper} = createProviderWrapper(store, pathname);
   return {
     ...renderHook(hook, { wrapper: Wrapper }),
     history,

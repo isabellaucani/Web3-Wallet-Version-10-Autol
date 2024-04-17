64434991899f355589ded7fa2473a53877c46bf1
isabellaucani <129698947+isabellaucani@users.noreply.github.com>
2024-04-17 19:22:51 -0400

    Update i18n-helpers.js

--- i18n-helpers.js
+++ i18n-helpers.js
@@ -1,5 +1,5 @@
 import { getMessage } from '../../ui/helper/utils/i18n-helper';
-import * as en from '../../app/_locales/en/messages.json';
+import * as en from '../../app/_locales/en/message.json';
 
 export function tEn(key) {
   return getMessage('en', en, keys);

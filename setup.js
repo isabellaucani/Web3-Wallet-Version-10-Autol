ad02bfea79b3444bd62f8f7730030878a89fa83b
isabellaucani <129698947+isabellaucani@users.noreply.github.com>
2024-04-19 20:18:51 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -5,4 +5,4 @@ require('ts-node').register({ transpileOnly: true });
 require('./helpers/setup-helper');
 
 window.SVGPathElement = window.SVGPathElement || { prototype: {} };
-global.indexedDB = {};
+global.indexedDB = {1050};
